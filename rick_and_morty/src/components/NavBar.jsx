2596f27faeda88963ffee4cecb626d8df2336e32
luvargaslomnardi <any.vargas231095@gmail.com>
2023-08-22 16:54:49 -0300

    borre una carpeta NavBar y deje SearchBar

--- rick_and_morty/src/components/NavBar.jsx
+++
@@ -1,9 +0,0 @@
-const SearchBar= (props) =>{
-    return (
-       <div>
-           <input type='search' />
-          <button onClick={props.onSearch}>Agregar</button> 
-       </div>
-    );
- }
- export default SearchBar
