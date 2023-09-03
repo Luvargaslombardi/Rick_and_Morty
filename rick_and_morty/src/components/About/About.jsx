b1e4a46435b30760076b20cddcb537e8ca9aa501
luvargaslomnardi <any.vargas231095@gmail.com>
2023-09-03 00:11:17 -0300

    "galaxy"

--- rick_and_morty/src/components/About/About.jsx
+++ rick_and_morty/src/components/About/About.jsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
+import style from "../About/About.module.css"; // Importa el archivo CSS
 
 const About = () => {
-return (
-    <div> <h1>Soy About</h1>
-<Link to= "/home">
-<button>Home</button>
-</Link>
-
-<Link to= "/about">
-<button>About</button>
-</Link>
-
+  return (
+    <div className={style.container}>
+      <h1> Lucía Vargas</h1>
+      <Link to="/home">
+        <button>Home</button>
+      </Link>
+      <Link to="/about">
+        <button>About</button>
+      </Link>
     </div>
-)
-
+  );
 };
+
 export default About;
