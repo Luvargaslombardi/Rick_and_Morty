d0931e0717e68a9e5e351352596fe196db9cca93
luvargaslomnardi <any.vargas231095@gmail.com>
2023-09-04 19:08:56 -0300

    Arreglos front

--- rick_and_morty/src/components/About/About.jsx
+++ rick_and_morty/src/components/About/About.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import style from "../About/About.module.css";
 import Card from "../Card/Card";
+import miFoto from "../../assets1/lu.png"
 
 const About = () => {
   return (
     <div className={style.container}>
       <h1>About me</h1>
       <Card
-        image= "https://scontent.ftuc1-1.fna.fbcdn.net/v/t39.30808-6/359825690_10230752708589628_4706988714971144698_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=49d041&_nc_eui2=AeEdX8kugn_MJEVrd5Qsh0PAczShUVVqb4xzNKFRVWpvjI-EIab1AmEMUWbmWdf0Jy0&_nc_ohc=UzLAE086ipgAX9wRh6A&_nc_ht=scontent.ftuc1-1.fna&oh=00_AfCueg6RqsqPlOo1PJ_2E25Qf74RCBwePKAk9Kp-89jWcQ&oe=64F9EB76"
+        image= {miFoto}
         name="Lucía Vargas"
         status= "Alive"
         species="Develloper"
